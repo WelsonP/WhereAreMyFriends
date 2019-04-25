0014b8486437eec8cb58f1f8a28e7cff33719c3f
Welson Pan <welson.pan@hotmail.com>
2019-04-25 13:14:46 -0700

    Sets up ARSCNView

--- WhereAreMyFriends/ViewController.swift
+++ WhereAreMyFriends/ViewController.swift
@@ -15,51 +15,43 @@ import SceneKit.ModelIO
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
-    
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Set the view's delegate
         sceneView.delegate = self
-        
+
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
+
         // Set the scene to the view
         sceneView.scene = scene
+        sceneView.antialiasingMode = .multisampling2X
+        view = sceneView
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
+        configuration.worldAlignment = .gravityAndHeading
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -75,4 +67,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
 
     }
+
+    private var sceneView: ARSCNView = {
+        let sceneView = ARSCNView(frame: .zero)
+        return sceneView
+    }()
 }
